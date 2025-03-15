ec72cfa940bf46536a08b516ade716d23c34c599
DESKTOP-LD7UE8O\BROTHERS <nadoarmando22@gmail.com>
2025-03-15 05:50:46 +0200

    Added authentication feature

---
+++ backend/app/Http/Controllers/NewPasswordController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+
+class NewPasswordController extends Controller
+{
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'token' => 'required|string',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+    
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                ])->setRememberToken(Str::random(60));
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+    
+        return $status === Password::PASSWORD_RESET
+            ? response()->json(['message' => trans($status)]
+            , 200)
+            : response()->json(['message' => trans($status)
+        ], 400);
+    }
+    
+}
